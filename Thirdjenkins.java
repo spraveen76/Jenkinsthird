36bb781ca98b4890c619c4c3c61a4897f30b31e6
spraveen76 <42091315+spraveen76@users.noreply.github.com>
2022-05-21 17:30:29 +0530

    Update Thirdjenkins.java

--- Thirdjenkins.java
+++ Thirdjenkins.java
@@ -5,5 +5,7 @@ public static void main(String[] args){
 system.out.println("This will print new message");
   //adding new line to check build output in jenkins
   system.out.println("This will print new message");
+    //adding new line to check build output in jenkins
+  system.out.println("This will print seond message");
 }
 }
