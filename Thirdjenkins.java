2e6dbad527ed44426f23286e8da62ea156629363
spraveen76 <42091315+spraveen76@users.noreply.github.com>
2022-05-21 17:25:41 +0530

    new sysout statement added

--- Thirdjenkins.java
+++ Thirdjenkins.java
@@ -3,5 +3,7 @@ public class ThirdJenkins{
 public static void main(String[] args){
 
 system.out.println("This will print new message");
+  //adding new line to check build output in jenkins
+  system.out.println("This will print new message");
 }
 }
